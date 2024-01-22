bfdca46c20cdde8899341613531763e46fd29d50
Oleh Helgi <povic@ukr.net>
2024-01-22 22:33:58 +0200

    modified:   03-files-in-folder/index.js

---
+++ 03-files-in-folder/index.js
@@ -0,0 +1,25 @@
+const fs = require('fs');
+const path = require('path');
+const folderPath = './03-files-in-folder/secret-folder';
+
+function displayFileInfo() {
+  fs.readdir(folderPath, { withFileTypes: true }, (err, files) => {
+    if (err) {
+      return;
+    }
+    files.forEach((file) => {
+      const filePath = path.join(folderPath, file.name);
+
+      fs.stat(filePath, (statErr, stats) => {
+        if (statErr) {
+          return;
+        }
+        if (file.isFile()) {
+          const fileSizeInKB = (stats.size / 1024).toFixed(2);
+          console.log(`${file.name}-${fileSizeInKB}kb`);
+        }
+      });
+    });
+  });
+}
+displayFileInfo();
